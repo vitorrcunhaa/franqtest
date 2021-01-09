c96d7f38d3439a940f97623b46accd39b8f12c2b
Vitor Cunha <vitorrcunhaa@gmail.com>
2021-01-08 23:59:28 -0300

    Add phone to custom user

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core.models import User
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'phone')
+    model = User
+
+
+admin.site.register(User, CustomUserAdmin)
